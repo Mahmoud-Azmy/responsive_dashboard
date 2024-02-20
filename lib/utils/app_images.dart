4bacda8b94d2c94104474ad95cf46c3e1a955080
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2024-02-20 02:36:58 +0200

    add assets images

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,63 @@
+class Assets {
+  Assets._();
+
+  /// Assets for imageBalance
+  /// assets/images/balance.svg
+  static const String imageBalance = "assets/images/balance.svg";
+
+  /// Assets for imageCardBackgroun
+  /// assets/images/card_backgroun.svg
+  static const String imageCardBackgroun = "assets/images/card_backgroun.svg";
+
+  /// Assets for imageDashboard
+  /// assets/images/dashboard.svg
+  static const String imageDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imageExpenses
+  /// assets/images/expenses.svg
+  static const String imageExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imageGallery
+  /// assets/images/gallery.svg
+  static const String imageGallery = "assets/images/gallery.svg";
+
+  /// Assets for imageIncomFrame
+  /// assets/images/incom_frame.svg
+  static const String imageIncomFrame = "assets/images/incom_frame.svg";
+
+  /// Assets for imageIncome
+  /// assets/images/income.svg
+  static const String imageIncome = "assets/images/income.svg";
+
+  /// Assets for imageLogout
+  /// assets/images/logout.svg
+  static const String imageLogout = "assets/images/logout.svg";
+
+  /// Assets for imageMyInvestments
+  /// assets/images/my_investments.svg
+  static const String imageMyInvestments = "assets/images/my_investments.svg";
+
+  /// Assets for imageMyTransaction
+  /// assets/images/my_transaction.svg
+  static const String imageMyTransaction = "assets/images/my_transaction.svg";
+
+  /// Assets for imageSettings
+  /// assets/images/settings.svg
+  static const String imageSettings = "assets/images/settings.svg";
+
+  /// Assets for imageStatistics
+  /// assets/images/statistics.svg
+  static const String imageStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imageUser1
+  /// assets/images/user1.svg
+  static const String imageUser1 = "assets/images/user1.svg";
+
+  /// Assets for imageUser2
+  /// assets/images/user2.svg
+  static const String imageUser2 = "assets/images/user2.svg";
+
+  /// Assets for imageWalletAccount
+  /// assets/images/wallet_account.svg
+  static const String imageWalletAccount = "assets/images/wallet_account.svg";
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/views/widgets/dash_board_desktop_layout.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashBoardDesktopLayout(),
+      ),
+    );
   }
 }
