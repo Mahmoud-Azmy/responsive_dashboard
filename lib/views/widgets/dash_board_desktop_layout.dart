e62c3a5fd59b33ad85346b47df4487ecc6b2a429
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2024-02-20 02:36:00 +0200

    create dash board desktop layout

---
+++ lib/views/widgets/dash_board_desktop_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DashBoardDesktopLayout extends StatelessWidget {
+  const DashBoardDesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
