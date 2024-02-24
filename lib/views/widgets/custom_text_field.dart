fbbdc71e62b7ad1ac860e45a3017b9256d2cf3b5
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2024-02-25 00:25:06 +0200

    custom text field

---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/text_styles.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hintText});
+  final String hintText;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle:
+            AppStyles.styleRegular16.copyWith(color: const Color(0xffAAAAAA)),
+        fillColor: const Color(0xffF1F1F1),
+        filled: true,
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: const BorderSide(
+        color: Color(0xffF1F1F1),
+      ),
+    );
+  }
+}
---
+++ lib/views/widgets/title_text_field.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/text_styles.dart';
+import 'package:responsive_dash_board/views/widgets/custom_text_field.dart';
+
+class TitleTextField extends StatelessWidget {
+  const TitleTextField(
+      {super.key, required this.title, required this.hintText});
+  final String title;
+  final String hintText;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: AppStyles.styleMedium16,
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        CustomTextField(
+          hintText: hintText,
+        )
+      ],
+    );
+  }
+}
