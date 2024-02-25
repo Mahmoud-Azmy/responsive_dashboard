83c4cced6724e9d0e8374a9eab1e011f2d81dea9
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2024-02-26 00:21:02 +0200

    my card page view

---
+++ lib/views/widgets/my_card_page_view.dart
@@ -0,0 +1,16 @@
+import 'package:expandable_page_view/expandable_page_view.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/my_card.dart';
+
+class MyCardPageView extends StatelessWidget {
+  const MyCardPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpandablePageView(
+        // animationDuration: const Duration(milliseconds: 300),
+        children: List.generate(3, (index) {
+      return const MyCard();
+    }));
+  }
+}
