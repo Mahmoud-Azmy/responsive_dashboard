9ebcb2c84a20de1a3aed57a436a302ab8fa8f119
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2024-08-27 08:33:20 +0300

    DashBoardMobilLayout

---
+++ lib/views/widgets/dash_board_mobile_layout.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/all_expenses.dart';
+import 'package:responsive_dash_board/views/widgets/income_section.dart';
+import 'package:responsive_dash_board/views/widgets/my_card_transaction_history_section.dart';
+import 'package:responsive_dash_board/views/widgets/quick_invoice.dart';
+
+class DashBoardMobilLayout extends StatelessWidget {
+  const DashBoardMobilLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 24,
+          ),
+          AllExpenses(),
+          SizedBox(
+            height: 14,
+          ),
+          QuickInvoice(),
+          SizedBox(
+            height: 24,
+          ),
+          MyCardTransactionHistorySection(),
+          SizedBox(
+            height: 24,
+          ),
+          IncomeSection(),
+        ],
+      ),
+    );
+  }
+}
